b3967be0f39d8f7a505c5acf5698cacf592390e8
mehrshaad <mehrdadashzadeh1379@gmail.com>
2024-06-12 12:00:25 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/jetReports/"
+  base: "./",
+  build: {
+    rollupOptions: {
+    },
+  },
 })
